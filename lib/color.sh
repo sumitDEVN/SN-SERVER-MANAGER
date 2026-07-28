fa812687b1934798acc88a594c10a2a246c7d5a9
sumitDEVN <143249428+sumitDEVN@users.noreply.github.com>
2026-07-28 17:51:14 +0600

    Update color.sh

--- lib/color.sh
+++ lib/color.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-# ========= COLORS =========
-
-RED='\033[1;31m'
-GREEN='\033[1;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[1;34m'
-PURPLE='\033[1;35m'
-CYAN='\033[1;36m'
-WHITE='\033[1;37m'
-
+# Reset
 NC='\033[0m'
+
+# Regular Colors
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+
+# Bold Colors
+BRED='\033[1;31m'
+BGREEN='\033[1;32m'
+BYELLOW='\033[1;33m'
+BBLUE='\033[1;34m'
+BPURPLE='\033[1;35m'
+BCYAN='\033[1;36m'
+BWHITE='\033[1;37m'
