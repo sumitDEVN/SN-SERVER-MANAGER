34dff8ee46ff180505a65f80803a6207016e8ab3
sumitDEVN <143249428+sumitDEVN@users.noreply.github.com>
2026-07-28 19:54:58 +0600

    Update logger.sh

--- lib/logger.sh
+++ lib/logger.sh
@@ -1,1 +1,32 @@
+#!/bin/bash
 
+# ==============================
+# Logger Library
+# ==============================
+
+source config/config.conf
+
+log() {
+    local level="$1"
+    local message="$2"
+
+    mkdir -p logs
+
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] [$level] $message" >> "$LOG_FILE"
+}
+
+log_info() {
+    log "INFO" "$1"
+}
+
+log_warning() {
+    log "WARNING" "$1"
+}
+
+log_error() {
+    log "ERROR" "$1"
+}
+
+log_success() {
+    log "SUCCESS" "$1"
+}
