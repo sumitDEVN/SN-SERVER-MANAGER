253725475d970cf2e080b1a6ca000c8cc9f12f10
sumitDEVN <143249428+sumitDEVN@users.noreply.github.com>
2026-07-28 14:36:33 +0600

    Update database.sh

--- lib/database.sh
+++ lib/database.sh
@@ -2,10 +2,29 @@
 
 DB_FILE="database/users.json"
 
-db_exists() {
+init_database() {
 
-if [ ! -f "$DB_FILE" ]; then
-    echo "[]" > "$DB_FILE"
-fi
+    if [ ! -f "$DB_FILE" ]; then
+        echo "[]" > "$DB_FILE"
+    fi
+
+}
+
+user_exists() {
+
+    local username="$1"
+
+    grep -q "\"username\":\"$username\"" "$DB_FILE"
+
+}
+
+save_demo_user() {
+
+    local username="$1"
+    local days="$2"
+
+    echo "---------------------------------" >> "$DB_FILE"
+    echo "username:$username" >> "$DB_FILE"
+    echo "days:$days" >> "$DB_FILE"
 
 }
