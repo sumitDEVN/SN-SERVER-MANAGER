b721134564e9bcc4a777ad60f84a2405cabc9602
sumitDEVN <143249428+sumitDEVN@users.noreply.github.com>
2026-07-28 14:41:23 +0600

    Update user.sh

--- lib/user.sh
+++ lib/user.sh
@@ -1,1 +1,30 @@
+#!/bin/bash
 
+create_user() {
+
+    local username="$1"
+    local password="$2"
+    local days="$3"
+    local type="$4"
+
+    save_demo_user "$username" "$days"
+
+}
+
+delete_user() {
+
+    echo "Delete User (Coming Soon)"
+
+}
+
+renew_user() {
+
+    echo "Renew User (Coming Soon)"
+
+}
+
+list_users() {
+
+    cat database/users.json
+
+}
