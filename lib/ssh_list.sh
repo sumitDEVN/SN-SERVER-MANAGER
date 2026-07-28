00425758e21bee63ea191055653be565a24566aa
sumitDEVN <143249428+sumitDEVN@users.noreply.github.com>
2026-07-28 19:33:45 +0600

    Update ssh_list.sh

--- lib/ssh_list.sh
+++ lib/ssh_list.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
 
+clear
+echo "=============================="
+echo "        SSH USER LIST"
+echo "=============================="
+echo
+
+printf "%-20s %-15s\n" "USERNAME" "EXPIRE DATE"
+echo "-------------------------------------------"
+
+while IFS=: read -r user _ uid _ _ _ shell
+do
+    if [ "$uid" -ge 1000 ] && [ "$shell" = "/bin/false" ]; then
+        expire=$(chage -l "$user" | grep "Account expires" | cut -d: -f2 | xargs)
+        printf "%-20s %-15s\n" "$user" "$expire"
+    fi
+done < /etc/passwd
+
+echo
+read -n1 -r -p "Press any key..."
