4463bc91aeefeb13bbea6d57bfa160b514c0198c
sumitDEVN <143249428+sumitDEVN@users.noreply.github.com>
2026-07-28 15:53:16 +0600

    Update dashboard.sh

--- lib/dashboard.sh
+++ lib/dashboard.sh
@@ -1,1 +1,31 @@
+#!/bin/bash
 
+dashboard() {
+
+title "SERVER STATUS"
+
+echo "🟢 Status   : Online"
+echo "👤 User     : $(whoami)"
+echo "💻 Hostname : $(hostname)"
+echo "🕒 Time     : $(date '+%d-%m-%Y %H:%M:%S')"
+
+echo
+title "RESOURCE"
+
+echo "🧠 RAM"
+
+free -h | awk '/Mem:/ {print "   Used : "$3" / "$2}'
+
+echo
+
+echo "💾 Disk"
+
+df -h / | awk 'NR==2 {print "   Used : "$3" / "$2" ("$5")"}'
+
+echo
+
+echo "⏱ Uptime"
+
+uptime -p
+
+}
