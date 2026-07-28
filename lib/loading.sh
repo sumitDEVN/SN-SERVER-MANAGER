7a95b8525f8d179816fb1c61992fa2ae342a187d
sumitDEVN <143249428+sumitDEVN@users.noreply.github.com>
2026-07-28 20:01:15 +0600

    Update loading.sh

--- lib/loading.sh
+++ lib/loading.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+loading() {
+
+    local msg="${1:-Loading}"
+
+    echo -ne "$msg "
+
+    for i in {1..5}
+    do
+        echo -ne "■"
+        sleep 0.15
+    done
+
+    echo " ✓"
+}
