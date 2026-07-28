bcdb2bc180d72bc870eca98c6f91266bd4a12df9
sumitDEVN <143249428+sumitDEVN@users.noreply.github.com>
2026-07-28 14:06:26 +0600

    Update create_ssh.sh

--- menu/create_ssh.sh
+++ menu/create_ssh.sh
@@ -4,23 +4,58 @@ create_ssh_user() {
 
 banner
 
-echo "========= CREATE SSH USER ========="
+echo -e "${CYAN}=========== CREATE SSH USER ===========${NC}"
 echo
 
-read -p "Username : " username
-read -s -p "Password : " password
-echo
-read -p "Valid Days : " days
+# Username
+while true
+do
+    read -p "Username : " username
 
-echo
-echo "==================================="
-echo "Username : $username"
-echo "Password : ********"
-echo "Valid For : $days Days"
-echo "==================================="
+    if [[ -z "$username" ]]; then
+        error "Username cannot be empty!"
+    else
+        break
+    fi
+done
 
-echo
-success "Data saved successfully (Demo Mode)"
+# Password
+while true
+do
+    read -s -p "Password : " password
+    echo
+
+    if [[ ${#password} -lt 6 ]]; then
+        error "Password must be at least 6 characters!"
+    else
+        break
+    fi
+done
+
+# Valid Days
+while true
+do
+    read -p "Valid Days : " days
+
+    if [[ ! "$days" =~ ^[0-9]+$ ]]; then
+        error "Only numbers are allowed!"
+    else
+        break
+    fi
+done
+
+clear
+banner
+
+echo -e "${GREEN}User Information${NC}"
+echo "--------------------------------------"
+echo "Username   : $username"
+echo "Password   : ********"
+echo "Valid Days : $days"
+echo "--------------------------------------"
+
+success "Demo user created successfully."
+
 pause
 
 }
