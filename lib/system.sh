86361dfa7af7c3bb81f72f9ff5a31c7b62da45f3
sumitDEVN <143249428+sumitDEVN@users.noreply.github.com>
2026-07-28 17:54:59 +0600

    v0.5 - Rebuild system module

--- lib/system.sh
+++ lib/system.sh
@@ -4,14 +4,6 @@ get_hostname() {
     hostname
 }
 
-get_kernel() {
-    uname -r
-}
-
-get_arch() {
-    uname -m
-}
-
 get_os() {
     if [ -f /etc/os-release ]; then
         . /etc/os-release
@@ -21,10 +13,30 @@ get_os() {
     fi
 }
 
+get_kernel() {
+    uname -r
+}
+
+get_arch() {
+    uname -m
+}
+
 get_uptime() {
     uptime -p
 }
 
 get_datetime() {
     date "+%d-%m-%Y %I:%M:%S %p"
+}
+
+get_ram() {
+    free -h | awk '/Mem:/ {print $3 " / " $2}'
+}
+
+get_disk() {
+    df -h / | awk 'NR==2 {print $3 " / " $2 " (" $5 ")"}'
+}
+
+get_cpu_load() {
+    awk '{print $1}' /proc/loadavg
 }
