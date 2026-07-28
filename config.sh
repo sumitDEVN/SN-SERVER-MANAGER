0722954f5e6abb0a7042761c10602d82a38ed5d1
sumitDEVN <143249428+sumitDEVN@users.noreply.github.com>
2026-07-28 22:02:41 +0600

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# ==================================
+# SN SERVER MANAGER v1.0
+# Configuration File
+# ==================================
+
+PANEL_NAME="SN SERVER MANAGER"
+
+VERSION="1.0"
+
+BASE_DIR="/root/SN-SERVER-MANAGER"
+
+USER_DB="$BASE_DIR/users/users.db"
+
+SSH_PORT="22"
+
+DATE=$(date +"%Y-%m-%d")
+
+HOSTNAME=$(hostname)
