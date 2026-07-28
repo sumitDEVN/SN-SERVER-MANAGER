7eddeb4c10edb5ca191e50b952c5d08418bf5504
sumitDEVN <143249428+sumitDEVN@users.noreply.github.com>
2026-07-28 12:46:18 +0600

    Update ssh.sh

--- menu/ssh.sh
+++ menu/ssh.sh
@@ -1,1 +1,66 @@
+#!/bin/bash
 
+source lib/color.sh
+source lib/banner.sh
+source lib/function.sh
+
+ssh_menu() {
+
+while true
+do
+
+banner
+
+echo -e "${CYAN}=========== SSH MANAGER ===========${NC}"
+echo
+echo -e "${GREEN}[1] Create SSH User${NC}"
+echo -e "${GREEN}[2] Delete SSH User${NC}"
+echo -e "${GREEN}[3] Renew SSH User${NC}"
+echo -e "${GREEN}[4] List SSH Users${NC}"
+echo -e "${GREEN}[5] Online SSH Users${NC}"
+echo -e "${RED}[0] Back${NC}"
+echo
+
+read -p "Select Menu : " sshmenu
+
+case $sshmenu in
+
+1)
+echo "Create SSH User (Coming Soon)"
+pause
+;;
+
+2)
+echo "Delete SSH User (Coming Soon)"
+pause
+;;
+
+3)
+echo "Renew SSH User (Coming Soon)"
+pause
+;;
+
+4)
+echo "User List (Coming Soon)"
+pause
+;;
+
+5)
+echo "Online Users (Coming Soon)"
+pause
+;;
+
+0)
+break
+;;
+
+*)
+error "Invalid Menu"
+pause
+;;
+
+esac
+
+done
+
+}
