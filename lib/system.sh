88c14995f1149d8c8e034da43fa8f512231f4ddf
sumitDEVN <143249428+sumitDEVN@users.noreply.github.com>
2026-07-28 15:47:49 +0600

    Update system.sh

--- lib/system.sh
+++ lib/system.sh
@@ -1,1 +1,30 @@
+#!/bin/bash
 
+get_hostname() {
+    hostname
+}
+
+get_kernel() {
+    uname -r
+}
+
+get_arch() {
+    uname -m
+}
+
+get_os() {
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        echo "$PRETTY_NAME"
+    else
+        uname -o
+    fi
+}
+
+get_uptime() {
+    uptime -p
+}
+
+get_datetime() {
+    date "+%d-%m-%Y %I:%M:%S %p"
+}
