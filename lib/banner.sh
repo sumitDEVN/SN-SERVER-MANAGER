cfe74012266aa407d9e9a6880e6f62f32a4b2776
sumitDEVN <143249428+sumitDEVN@users.noreply.github.com>
2026-07-28 12:43:25 +0600

    Update banner.sh

--- lib/banner.sh
+++ lib/banner.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+banner(){
+
+clear
+
+echo -e "${CYAN}"
+echo "======================================================"
+echo "             SN SERVER MANAGER"
+echo "======================================================"
+echo -e "${WHITE} Version : 1.0"
+echo " Developer : Sumit"
+echo "======================================================"
+echo -e "${NC}"
+
+}
