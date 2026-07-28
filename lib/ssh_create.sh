bbb88c25077b3caa7ad58f66d49fcd4163c96c2a
sumitDEVN <143249428+sumitDEVN@users.noreply.github.com>
2026-07-28 19:31:16 +0600

    Update ssh_create.sh

--- lib/ssh_create.sh
+++ lib/ssh_create.sh
@@ -1,1 +1,44 @@
+#!/bin/bash
 
+clear
+echo "=============================="
+echo "      CREATE SSH USER"
+echo "=============================="
+echo
+
+read -p "Username : " username
+
+# Username exists?
+if id "$username" &>/dev/null; then
+    echo
+    echo "User already exists!"
+    read -n1 -r -p "Press any key..."
+    exit
+fi
+
+read -s -p "Password : " password
+echo
+read -p "Expired (Days) : " days
+
+# Default value
+[ -z "$days" ] && days=30
+
+expire_date=$(date -d "+$days days" +"%Y-%m-%d")
+
+useradd -e "$expire_date" -M -s /bin/false "$username"
+
+echo "${username}:${password}" | chpasswd
+
+clear
+echo "=============================="
+echo "   SSH ACCOUNT CREATED"
+echo "=============================="
+echo
+echo "Username     : $username"
+echo "Password     : $password"
+echo "Expired Date : $expire_date"
+echo
+echo "Status : SUCCESS"
+echo
+
+read -n1 -r -p "Press any key..."
