11eefd8ff80ab0d2876f3fce7ce61353b40b6afa
sumitDEVN <143249428+sumitDEVN@users.noreply.github.com>
2026-07-28 19:36:21 +0600

    Update ssh_info.sh

--- lib/ssh_info.sh
+++ lib/ssh_info.sh
@@ -1,1 +1,40 @@
+#!/bin/bash
 
+clear
+echo "=============================="
+echo "      SSH USER INFORMATION"
+echo "=============================="
+echo
+
+read -p "Username : " username
+
+# User exists?
+if ! id "$username" &>/dev/null; then
+    echo
+    echo "User not found!"
+    echo
+    read -n1 -r -p "Press any key..."
+    exit
+fi
+
+expire=$(chage -l "$username" | grep "Account expires" | cut -d: -f2 | xargs)
+lastpass=$(chage -l "$username" | grep "Last password change" | cut -d: -f2 | xargs)
+lastlogin=$(lastlog -u "$username" | tail -n 1)
+
+clear
+echo "=============================="
+echo "      USER INFORMATION"
+echo "=============================="
+echo
+echo "Username             : $username"
+echo "UID                  : $(id -u "$username")"
+echo "Home Directory       : $(getent passwd "$username" | cut -d: -f6)"
+echo "Shell                : $(getent passwd "$username" | cut -d: -f7)"
+echo "Account Expires      : $expire"
+echo "Last Password Change : $lastpass"
+echo
+echo "Last Login:"
+echo "$lastlogin"
+echo
+
+read -n1 -r -p "Press any key..."
