098ffe497a4dd41a07a21847ba0dc388a032e54b
sumitDEVN <143249428+sumitDEVN@users.noreply.github.com>
2026-07-28 16:12:13 +0600

    Update delete_ssh.sh

--- menu/delete_ssh.sh
+++ menu/delete_ssh.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+delete_ssh_user() {
+
+banner
+
+title "DELETE SSH USER"
+
+read -p "Username : " username
+
+echo
+echo "Demo Mode"
+
+echo "User '$username' will be deleted."
+
+pause
+
+}
