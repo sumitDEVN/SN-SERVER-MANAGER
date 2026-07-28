2faf1db253ed7c3911012f07c5ef9adb0e6593fb
sumitDEVN <143249428+sumitDEVN@users.noreply.github.com>
2026-07-28 12:49:12 +0600

    Update create_ssh.sh

--- menu/create_ssh.sh
+++ menu/create_ssh.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
 
+create_ssh_user() {
+
+banner
+
+echo "========= CREATE SSH USER ========="
+echo
+
+read -p "Username : " username
+read -s -p "Password : " password
+echo
+read -p "Valid Days : " days
+
+echo
+echo "==================================="
+echo "Username : $username"
+echo "Password : ********"
+echo "Valid For : $days Days"
+echo "==================================="
+
+echo
+success "Data saved successfully (Demo Mode)"
+pause
+
+}
